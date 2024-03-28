bcb17e4a2ffa1312b4dcf58faaddd6c428c90d63
Worley Sacramento <78088577+WorleySacramento@users.noreply.github.com>
2024-03-28 14:31:21 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { IoConstructOutline } from "react-icons/io5";
 import { AiFillProject } from 'react-icons/ai'
 import React, { useRef } from "react";
 import ImageDiamond from '@/public/images/LogoPrincipal.png'
 
 
-// const inter = Inter({ subsets: ['latin'] })
-
-
-
 export default function Home() {
-
 
   return (
     <>
